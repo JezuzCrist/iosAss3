4a383a18a8aef1d4e07da929bd975492b3d25188
macDevMachinVB <devmachin@macDevMachinVBs-MacBook-Pro.local>
2016-12-28 00:38:20 -0800

    first commit

---
+++ assiment3/assiment3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  assiment3
+//
+//  Created by macDevMachinVB on 27/12/2016.
+//  Copyright © 2016 macDevMachinVB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
